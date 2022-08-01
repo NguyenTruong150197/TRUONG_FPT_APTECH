5acab0098646a49f6c5ba337b40d07db190954a1
NguyenTruong150197 <truong12121996@gmail.com>
2022-08-01 21:07:17 +0700

    sua doi

--- Bai6/bai6(bt1).cpp
+++ Bai6/bai6(bt1).cpp
@@ -3,28 +3,36 @@ int main(){
 	int n;
 	printf("Nha so nguyen n = ");
 	scanf("%d", &n);
-	if(n <= 0){
-		printf("Nhap sai, moi chay lai chuong trinh!");
+
+	int ary[n];
+	int i;
+	printf("Nhap %d so nguyen:\n", n);
+	for(i = 0; i < n; i++){
+		scanf("%d", &ary[i]);
+	} 
+	int high1 = ary[0];
+	for(i = 0; i < n; i++){
+		if(ary[i] > high1){
+			high1 = ary[i];
+		}
+	}
+	int high2 = high1;
+	for(i = 0; i < n; i++){
+		if(ary[i] < high1){
+			high2 = ary[i];
+			break;
+		}
+	}
+	if(high2 == high1){
+		printf("Mang khong co gia tri gan lon nhat\n");
 	}
 	else{
-		int ary[n];
-		int i, high1, high2;
-		printf("Nhap %d so nguyen:\n", n);
 		for(i = 0; i < n; i++){
-			scanf("%d", &ary[i]);
-		} 
-		high1 = high2 = ary[0];
-		for(i = 1; i < n; i++){
-			if(ary[i] > high1){
-				high1 = ary[i];
-			}
-		}
-		for(i = 1; i < n; i++){
-			if(ary[i] > high2 && ary[i] != high1){
+			if(ary[i] > high2 && ary[i] < high1){
 				high2 = ary[i];
 			}
 		}
-		printf("So lon nhat trong mang la: %d\n", high1);
-		printf("So gan lon nhat la: %d", high2);
 	}
+	printf("So lon nhat trong mang la: %d\n", high1);
+	printf("So gan lon nhat la: %d", high2);
 }
